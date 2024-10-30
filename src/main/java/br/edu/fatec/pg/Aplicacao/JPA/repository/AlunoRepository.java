7ef53ac822ebd0411f5d8b99d0e35f82a79a6ff8
GlhermePereira <guilherne.aqw24@gmail.com>
2024-10-29 21:15:36 -0300

    versao final

--- src/main/java/br/edu/fatec/pg/Aplicacao/JPA/repository/AlunoRepository.java
+++
@@ -1,5 +0,0 @@
-package br.edu.fatec.pg.Aplicacao.JPA.repository;
-import br.edu.fatec.pg.Aplicacao.JPA.models.*;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlunoRepository extends JpaRepository<Aluno, Long> {}
